72b620f8b43fcb46b282485d8cf4e6f77562f9f2
Malwareus <marianaimpasse@gmail.com>
2023-01-25 15:39:03 +0300

    VatanGame Eklendi / Felis'i update etmiyor.

--- app.js
+++ app.js
@@ -75,16 +75,16 @@ io.on("connection", async (socket) => {
     socket.emit("GameSatisData", resGms.rows);
 
     //Sunucu isimleri çekiliyor.
-    const sunucuID = await pool.query("SELECT prices.server_id, servers.name FROM prices JOIN servers ON prices.server_id = servers.server_id WHERE site_id = 1 ;")
+    const sunucuID = await pool.query("SELECT prices.server_id, servers.name FROM prices JOIN servers ON prices.server_id = servers.server_id WHERE site_id = 1 ORDER BY servers.server_id ASC ;")
     socket.emit("serverNames", sunucuID.rows);
 
 
-// ID SORUNSALI, VERİTABANINA FARKLI ID İLE KAYIT EDİYOR ID'LERİ EŞLEŞTİRMEK LAZIM
+    //Bu bölümde KlasGame'den gelen fiyat ve server isimleri yakalanıyor ve veri tabanında güncelleniyor.
 
     socket.on("klasGamePrices", async (data) => {
             for (let i = 0; i < data.length; i++) {
-                const queryText = `UPDATE prices SET buy_price = $1, sell_price = $2 WHERE server_id = $3 AND (site_id= 2)`;
-                const values = [data[i].klasGameBuy, data[i].klasGameSell, i + 1];
+                const queryText = `UPDATE prices SET buy_price = $1, sell_price = $2 WHERE server_id = (SELECT server_id FROM servers WHERE name = $3) AND (site_id= 2)`;
+                const values = [data[i].klasGameBuy, data[i].klasGameSell, data[i].klasGameName];
                 try {
                     const res = await pool.query(queryText, values);
                 } catch (err) {
@@ -94,8 +94,40 @@ io.on("connection", async (socket) => {
     });
 
 
-    const resKg = await pool.query("SELECT * FROM prices WHERE site_id = 2");
-    socket.emit("KlasGameData", resKg.rows, result);
+    const resKg = await pool.query("SELECT * FROM prices WHERE site_id = 2 ORDER BY server_id ASC");
+    socket.emit("KlasGameData", resKg.rows);
+
+    socket.on("oyunEksPrices", async (data) => {
+        for (let i = 0; i < data.length; i++) {
+            const queryText = `UPDATE prices SET buy_price = $1, sell_price = $2 WHERE server_id = (SELECT server_id FROM servers WHERE name = $3) AND (site_id= 3)`;
+            const values = [data[i].oyunEksBuy, data[i].oyunEksSell, data[i].oyunEksName];
+            try {
+                const res = await pool.query(queryText, values);
+            } catch (err) {
+                console.log(err.stack);
+            }
+        }
+    });
+
+    const resOe = await pool.query("SELECT * FROM prices WHERE site_id = 3 ORDER BY server_id ASC");
+    socket.emit("oyunEksData", resOe.rows);
+
+
+    socket.on("vatanGamePrices", async (data) => {
+        for (let i = 0; i < data.length; i++) {
+            const queryText = `UPDATE prices SET buy_price = $1, sell_price = $2 WHERE server_id = (SELECT server_id FROM servers WHERE name = $3) AND (site_id= 4)`;
+            const values = [data[i].vatanGameBuyPriceClean, data[i].vatanGameSellPriceClean, data[i].vatanGameName];
+            try {
+                const res = await pool.query(queryText, values);
+                console.log(res.rowCount)
+            } catch (err) {
+                console.log(err.stack);
+            }
+        }
+    });
+
+    const resVg = await pool.query("SELECT * FROM prices WHERE site_id = 4 ORDER BY server_id ASC");
+    socket.emit("vatanGameData", resVg.rows);
 
 });
 
